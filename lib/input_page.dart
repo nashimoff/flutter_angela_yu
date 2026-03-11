6bbf1e4629e89f797e49d78990fc8c1f5288d37e
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-11 14:12:05 +0600

    a

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
-import 'reusable_card.dart'; //ashimoff nurlan
+import 'reusable_card.dart';
+import 'constants.dart';
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const bottomContainerColour = Color(0xFFEB1555);
+
 
 enum Gender{
   male,
@@ -65,6 +63,9 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               colour: activeCardColour,
+              cardChild: Column(
+                children: <Widget>[Text('HEIGHT')],
+              ),
             ),
           ),
           Expanded(
