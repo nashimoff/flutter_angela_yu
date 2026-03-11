d5ed0195fb09490a8d2682ed8ec0908ba7ae3f34
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-03-11 13:42:22 +0600

    a

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
-import 'reusable_card.dart'; //ashimoff
+import 'reusable_card.dart'; //ashimoff nurlan
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
