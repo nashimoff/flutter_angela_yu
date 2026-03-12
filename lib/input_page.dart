dcc097b554ea988b9ffef3ce3524929611638205
Nurlan Ashimov <nashimoff@gmail.com>
2026-03-13 00:43:38 +0600

    l

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,6 +18,7 @@ class _InputPageState extends State<InputPage> {
   Gender? selectedGender;
   int height = 180;
   int weight = 60;
+  int age = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -131,18 +132,24 @@ class _InputPageState extends State<InputPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          RoundIconButton(),
+                          RoundIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {
+                              setState(() {
+                                weight--;
+                              });
+                            },
+                          ),
                           SizedBox(
                             width: 10.0,
                           ),
-                          FloatingActionButton(
-                            shape: CircleBorder(),
-                            backgroundColor: Color(0xFF4C4F5E),
-                            onPressed: () {}, //eklendi
-                            child: Icon(
-                              Icons.add,
-                              color: Colors.white,
-                            ),
+                          RoundIconButton(
+                            icon: FontAwesomeIcons.plus,
+                            onPressed: () {
+                              setState(() {
+                                weight++;
+                              });
+                            },
                           ),
                         ],
                       ),
@@ -153,6 +160,44 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: ReusableCard(
                   colour: kActiveCardColour,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'AGE',
+                        style: kLabelTextStyle,
+                      ),
+                      Text(
+                        age.toString(),
+                        style: kNumberTextStyle,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          RoundIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: () {
+                              setState(
+                                () {
+                                  age--;
+                                },
+                              );
+                            },
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              })
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -170,16 +215,18 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RoundIconButton extends StatelessWidget {
-  RoundIconButton({this.child});
+  RoundIconButton(
+      {required this.icon, required this.onPressed}); //required eklendi
 
-  final Widget child;
+  final IconData icon;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      child: child,
-      onPressed: () {},
-      elevation: 6.0,
+      elevation: 0.0,
+      child: Icon(icon),
+      onPressed: onPressed as VoidCallback, //as VoidCallback eklendi
       constraints: BoxConstraints.tightFor(
         width: 56.0,
         height: 56.0,
