a5162e76ae3e452267adaaf0a4a1608c5343ae9d
Nurlan Ashimov <nashimoff@gmail.com>
2026-03-12 21:33:15 +0600

    l

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -85,12 +85,19 @@ class _InputPageState extends State<InputPage> {
                     ],
                   ),
                   SliderTheme(
-                    data: SliderTheme.of(context).copyWith(),
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
                     child: Slider(
                       value: height.toDouble(),
                       min: 120.0,
                       max: 220.0,
-                      activeColor: Colors.white,  //(0xFFEB1555)
                       inactiveColor: Color(0xFF8D8E98),
                       onChanged: (double newValue) {
                         setState(() {
